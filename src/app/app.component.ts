788001a70252ae7f56bb74f2f1bdbf2878d8edab
Fred-Rezende <raizen.fred@gmail.com>
2024-11-09 14:12:52 -0300

    Alteração de CSS PokeCard, alteração de como pegar pokemon name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+// import { PokeCardComponent } from './components/poke-card/poke-card.component';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pokedex';
-  numbers: number[] = Array.from({ length: 30 }, (_, i) => i + 1);
+  numbers: number[] = Array.from({ length: 1025 }, (_, i) => i + 1);
+  displayedImages: number[] = [];
+  imagesPerPage: number = 12;
+  currentPage: number = 0;
 
-  ngOnInit(): void { 
-    
+  // @ViewChild(PokeCardComponent) pokecardComponent!: PokeCardComponent;
+
+  constructor() {
+    this.loadMoreImages();
   }
+
+  ngOnInit(): void {
+    // this.loadMoreImages();
+  }
+
+  get hasMoreImages(): boolean {
+    return this.currentPage * this.imagesPerPage < this.numbers.length;
+  }
+
+  loadMoreImages(): void {
+    const nextPageImages = this.numbers.slice(
+      this.currentPage * this.imagesPerPage,
+      (this.currentPage + 1) * this.imagesPerPage
+    );
+    this.displayedImages = [...this.displayedImages, ...nextPageImages];
+    this.currentPage++;
+  }
+
+  searchPokemon(n?: number): void {
+    if (n === undefined) {
+      this.resetLoadPokemon(); // Reseta caso não haja termo de pesquisa
+    } else {
+      this.numbers = [n]; // Aplica a pesquisa
+      this.displayedImages = [];
+      this.currentPage = 0;
+      this.loadMoreImages();
+    }
+    
+    // this.numbers = [n]
+    // this.displayedImages = []
+    // this.currentPage = 0;
+    // this.loadMoreImages();
+    
+   
+  }
+
+  resetLoadPokemon(): void {
+    this.numbers = Array.from({ length: 1025 }, (_, i) => i + 1); // Restaura todos os números
+    this.displayedImages = []; // Limpa as imagens exibidas
+    this.currentPage = 0; // Reinicia a contagem de páginas
+    this.loadMoreImages(); // Carrega as primeiras imagens novamente
+  }
+  // searchPokemon(name: string) {
+  //   this.pokecardComponent.getPokemon(name);
+  //   console.log(this.pokecardComponent.getPokemon(name))
+  //   // this.numbers = []
+  //   this.displayedImages = []
+  //   this.currentPage = 0;
+  //   this.loadMoreImages();
+    
+  // }
+
+
+
 }
--- src/app/models/pokemonData.ts
+++ src/app/models/pokemonData.ts
@@ -1,19 +1,21 @@
 export type PokemonData = {
-    id:number
+  id:number
+  species: {
     name:string
-    sprites: {
-        front_default: string
-        other: {
-          "official-artwork": {
-            front_default: string
-          }
+  }
+  sprites: {
+      front_default: string
+      other: {
+        "official-artwork": {
+          front_default: string
         }
       }
-      types:{
-        slot: number
-        type: {
-          name: string
-          url: string
-        }
-      }[]
+    }
+    types:{
+      slot: number
+      type: {
+        name: string
+        url: string
+      }
+    }[]
 }
